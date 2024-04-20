a62831651d313e580609b352c364af4805d9dcac
John Bartucz <john@bartucz.com>
2024-04-19 19:35:12 -0500

    it's working!

--- app.py
+++ app.py
@@ -13,39 +13,49 @@ app.secret_key = getenv('FLASK_SECRET_KEY')
 client = OpenAI()
 client.api_key = getenv('OPENAI_API_KEY')
 
-messages = []
+prompt_context = getenv('PROMPT_CONTEXT')
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    global messages
+
+    if 'messages' not in session:
+        print("*** SETTING MESSAGES ***")
+        session['messages'] = []
+    if 'conversation' not in session:
+        print("*** SETTING CONVERSATION ***")
+        session['conversation'] = []
+        
     if request.method == 'POST':
         if 'reset' in request.form:
-            messages = []
-            session.pop('conversation', None)
+            print("*** RESETTING MESSAGES ***")
+            session['messages'] = []
+            print("*** RESETTING CONVERSATION ***")
+            session['conversation'] = []
             return redirect(url_for('home'))
 
         user_input = request.form.get('user_input')
         if user_input:
-            prompt = user_input + "\n\nYou are a friendly CS tutor. If the user tries to ask for the solution to a coding problem, do not provide the answer in code. Instead suggest ways to approach the problem."
+            prompt = prompt_context + user_input 
 
-            # only send the last 5 messages to avoid hallucination
-            messages.append({"role": "user", "content": prompt})
-            if len(messages) > 5:
-                messages.pop(0)
+            # only send the last 5 messages to avoid hallucination 
+            session['messages'].append({"role": "user", "content": prompt})
+            if len(session['messages']) > 5:
+                session['messages'].pop(0)
 
             chat_completion = client.chat.completions.create(
-                messages=messages,
+                messages=session['messages'],
                 model="gpt-4-turbo",
                 # Sets the sampling temperature to control randomness (0 makes output deterministic, 1 maximizes randomness, with 0.2 being more focused and less random).
                 temperature=0.2,
                 stream=False 
             )
-            if 'conversation' not in session:
-                session['conversation'] = []
+
+            print(f"\nquestion: {user_input} -- answer: {chat_completion.choices[0].message.content[:20]}")
+
             session['conversation'].append({'question': user_input, 'answer': chat_completion.choices[0].message.content})
+            print(f"session['conversation']: {session['conversation']}")
+            session.modified = True
 
-
-    
     return render_template('index.html', conversation=session.get('conversation', []))
 
 if __name__ == '__main__':
